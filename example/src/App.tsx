d5e59341336e063f1d474cb78e3c7037072cc4ee
Letty <login@letty.io>
2023-03-23 16:59:35 +0100

    feat: basic example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,41 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-editor';
+import {
+  actions,
+  multiply,
+  RichEditor,
+  RichToolbar,
+} from 'react-native-editor';
+
+const imageList = [
+  'https://upload.wikimedia.org/wikipedia/commons/5/54/Rayleigh-Taylor_instability.jpg',
+  'https://upload.wikimedia.org/wikipedia/commons/1/1a/Carnabotnet_geovideo_lowres.gif',
+  'https://img.lesmao.vip/k/h256/R/MeiTu/1297.jpg',
+  'https://img.lesmao.vip/k/h256/R/MeiTu/1292.jpg',
+]
+
+const initHTML = `<br/>
+<center><b onclick="_.sendEvent('TitleClick')" id="title" contenteditable="false">Rich Editor</b></center>
+<center>
+<a href="https://github.com/wxik/react-native-rich-editor">React Native</a>
+<span>And</span>
+<a href="https://github.com/wxik/flutter-rich-editor">Flutter</a>
+</center>
+<br/>
+<div><center><img src="${imageList[0]}" onclick="_.sendEvent('ImgClick')" contenteditable="false" height="170px"/></center></div>
+<pre type="javascript"><code>const editor = ReactNative;</code><code>console.log(editor);</code></pre>
+<br/>Click the picture to switch<br/><br/>
+<div>
+<img src="${imageList[0]}" onclick="_.sendEvent('ImgClick')" contenteditable="false" height="170px"/>
+<img src="${imageList[1]}" onclick="_.sendEvent('ImgClick')" contenteditable="false"/>
+</div>
+`;
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  const [result, setResult] = React.useState<number | undefined>()
+  const [content, setContent] = React.useState<string | undefined>(initHTML)
+  const richText = React.createRef() || useRef()
 
   React.useEffect(() => {
     multiply(3, 7).then(setResult);
@@ -13,6 +44,34 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Result: {result}</Text>
+      <View style={styles.textContainer}>
+        <RichEditor
+          editorStyle={{
+            contentCSSText: `
+              font-size: 16px;
+            `,
+          }}
+          initialContentHTML={content}
+          onChange={setContent}
+          // pasteAsPlainText
+          placeholder={'Schreibe den Nachrichtentext (mind. 14 Zeichen).'}
+          ref={richText}
+          scrollEnabled={true}
+          useContainer={false}
+        />
+        <RichToolbar
+          actions={[
+            actions.setBold,
+            actions.setItalic,
+            actions.insertBulletsList,
+            actions.insertOrderedList,
+          ]}
+          editor={richText}
+          iconSize={20}
+          reference={richText}
+          selectedIconTint={'pink'}
+        />
+      </View>
     </View>
   );
 }
@@ -22,6 +81,11 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textContainer: {
+    backgroundColor: '#f4e2fc',
+    height: 400,
+    width: '100%',
   },
   box: {
     width: 60,
