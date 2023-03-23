a9c7ca1c6959eafc1caaeab46327537628627ce1
Letty <login@letty.io>
2023-03-23 15:36:09 +0100

    chore: initial commit

---
+++ android/src/main/java/com/editor/EditorPackage.kt
@@ -0,0 +1,17 @@
+package com.editor
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+
+class EditorPackage : ReactPackage {
+  override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+    return listOf(EditorModule(reactContext))
+  }
+
+  override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+    return emptyList()
+  }
+}
---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  EditorExample
+//
+
+import Foundation
