193162a7957dd070c7ce2914906f4023cc0c0b27
Letty <login@letty.io>
2023-03-27 13:18:26 +0200

    feat: editor with types

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import RichEditor from './RichEditor'
 import RichToolbar, { defaultActions } from './RichToolbar';
-import {actions} from './const'
-import {createHTML, getContentCSS} from './editor'
+import { actions } from './const'
+import {
+  createHTML,
+  getContentCSS,
+} from './editor'
 
 export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a * b);
