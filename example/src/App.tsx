6cf9f7ac8d75bb88eda75eb1bd34c9959961dd04
Letty <login@letty.io>
2023-03-31 10:23:19 +0200

    refactor: clean example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,13 +1,11 @@
-import * as React from 'react';
-
-import { StyleSheet, View, Text } from 'react-native';
+import * as React from 'react'
+import { StyleSheet, View, Text } from 'react-native'
 import {
   actions,
-  multiply,
   RichEditor,
   RichToolbar,
-} from 'react-native-editor';
-import { RichEditorRef } from 'src/types';
+} from 'react-native-editor'
+import type { RichEditorRef } from 'src/types'
 
 const iconDict =  {
   'loveAction': require('./heart-solid.png'),
@@ -21,47 +19,33 @@ const iconDict =  {
 const imageList = [
   'https://upload.wikimedia.org/wikipedia/commons/5/54/Rayleigh-Taylor_instability.jpg',
   'https://upload.wikimedia.org/wikipedia/commons/1/1a/Carnabotnet_geovideo_lowres.gif',
-  'https://img.lesmao.vip/k/h256/R/MeiTu/1297.jpg',
-  'https://img.lesmao.vip/k/h256/R/MeiTu/1292.jpg',
 ]
 
 const initHTML = `<br/>
-<center><b onclick="_.sendEvent('TitleClick')" id="title" contenteditable="false">Rich Editor</b></center>
+<center><b id="title" contenteditable="false">Rich Editor</b></center>
 <center>
-<a href="https://github.com/wxik/react-native-rich-editor">React Native</a>
-<span>And</span>
-<a href="https://github.com/wxik/flutter-rich-editor">Flutter</a>
-</center>
-<br/>
-<div><center><img src="${imageList[0]}" onclick="_.sendEvent('ImgClick')" contenteditable="false" height="170px"/></center></div>
+<div><center><img src="${imageList[0]}" contenteditable="false"/></center></div>
 <pre type="javascript"><code>const editor = ReactNative;</code><code>console.log(editor);</code></pre>
-<br/>Click the picture to switch<br/><br/>
 <div>
-<img src="${imageList[0]}" onclick="_.sendEvent('ImgClick')" contenteditable="false" height="170px"/>
-<img src="${imageList[1]}" onclick="_.sendEvent('ImgClick')" contenteditable="false"/>
+<img src="${imageList[1]}" contenteditable="false"/>
 </div>
 `;
 
+const toolbarActions = [
+  actions.setBold,
+  actions.setItalic,
+  actions.insertBulletsList,
+  actions.insertOrderedList,
+  'loveAction',
+  'otherAction'
+]
+
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>()
   const [content, setContent] = React.useState<string | undefined>(initHTML)
   const richText = React.createRef<RichEditorRef>()
-  const [toolbarActions, setToolbarActions] = React.useState([
-    actions.setBold,
-    actions.setItalic,
-    actions.insertBulletsList,
-    actions.insertOrderedList,
-    'loveAction',
-    'otherAction'
-  ])
-
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
       <View style={styles.textContainer}>
         <RichEditor
           editorStyle={{
