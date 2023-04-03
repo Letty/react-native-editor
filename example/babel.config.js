a6b2b5358a0ca6503ecde7b0f8700ab2fc1c6a8b
Letty <login@letty.io>
2023-04-03 12:34:13 +0200

    fix: alias to work on android

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const pak = require('../package.json');
 
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
@@ -9,7 +8,7 @@ module.exports = {
       {
         extensions: ['.tsx', '.ts', '.js', '.json'],
         alias: {
-          [pak.name]: path.join(__dirname, '..', pak.source),
+          'react-native-editor': path.resolve('..'),
         },
       },
     ],
